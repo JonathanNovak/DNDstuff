ef50871baccf59c521e49fc5af722180beeb0ee0
JonathanNovak <41155961+JonathanNovak@users.noreply.github.com>
2018-07-12 15:37:55 -0400

    Update autochar.py

--- autochar.py
+++ autochar.py
@@ -56,12 +56,12 @@ def char_class(class_num):
 
 def race(race_num):
     switch = {
-        1:  "Human",
-        2:  "Gnome",
-        3:  "Dwarf",
-        4:  "Halfling",
+        1:  "Dwarf",
+        2:  "Elf",
+        3:  "Halfling",
+        4:  "Human",
         5:  "Dragonborn",
-        6:  "Elf",
+        6:  "Gnome",
         7:  "Half-Elf",
         8:  "Half-orc",
         9:  "Tiefling"
@@ -109,17 +109,17 @@ def char_feature(classname,level):
     classfeat=random.randint(class_f_range,class_f_range2)
 
     r_feat = requests.get('http://www.dnd5eapi.co/api/features/%s'%(str(classfeat)))
-    feat = json.loads(r_feat.content.decode('utf-8'))
+    d_feat = json.loads(r_feat.content.decode('utf-8'))
 
-    while(int(feat["level"]) > int(level)):
+    while(int(d_feat["level"]) > int(level)):
         classfeat=random.randint(class_f_range,class_f_range2)
         r_feat = requests.get('http://www.dnd5eapi.co/api/features/%s'%(str(classfeat)))
-        feat = json.loads(r_feat.content.decode('utf-8'))
+        d_feat = json.loads(r_feat.content.decode('utf-8'))
 
-    print ("FEAT: " + str(feat["name"]))
-    print ("desc: " + str(feat["desc"]))
+    print ("FEAT: " + str(d_feat["name"]))
+    print ("desc: " + str(d_feat["desc"]))
 
-def stats():
+def stats(race):
     statlist = []
     for x in range(6):
         roll1 = random.randint(1,7)
@@ -154,12 +154,34 @@ def otherstats(classname,level):
     else:
         return basehp
 
+def alignment():
+    alignment_num = random.randint(1,3)
+    switch = {
+        1:  "Lawful",
+        2:  "Neutral",
+        3:  "Chaotic",
+    }
+    alignment1 = str(switch.get(alignment_num, "null"))
+
+    alignment_num = random.randint(1,3)
+    switch = {
+        1:  "Good",
+        2:  "Neutral",
+        3:  "Evil",
+    }
+    alignment2 = str(switch.get(alignment_num, "null"))
+    return alignment1 + alignment2
+
 classname= char_class(class_num)
 race = race(race_num)
+r_race = requests.get('http://www.dnd5eapi.co/api/races/%s'%(str(race_num)))
+d_race = json.loads(r_race.content.decode('utf-8'))
 print (race)
-print (classname)
+
+print ("Class: " + classname)
+print("Alignment: " + alignment())
 char_feature(classname,level)
-print (stats())
+print (stats(race_num))
 print ("HP: "+ str(otherstats(classname,level)))
 r_spells = requests.get('http://dnd5eapi.co/api/spells/1')
 data = json.loads(r_spells.content.decode('utf-8'))
